5d602c40e9122600f83f63a0e2f23379d3cdcfa7
JinksPeng <jinkspeng@gmail.com>
2014-12-10 13:52:01 +0800

    add learn

---
+++ HTML5程序设计/app.js
@@ -0,0 +1,11 @@
+var express = require('express');
+
+var app = express.createServer();
+
+app.use(express.static('public'));
+
+app.get('/', function (req, res) {
+	res.render('index.html');
+});
+
+app.listen(3000);
